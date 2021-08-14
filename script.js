aa39eea820a77ae24596ee9073bdaa8e2c60db7b
Isay Zetter <59151193+izetter@users.noreply.github.com>
2021-08-13 19:51:02 -0500

    Circle trim animation functionality complete

--- script.js
+++ script.js
@@ -3,15 +3,23 @@
 const input = document.querySelector('input');
 const startPauseBtn = document.querySelector('#start-pause');
 const resetBtn = document.querySelector('#reset');
-const timeRemaining = null;
+const circle = document.querySelector('circle');
+const CIRCUMFERENCE = circle.getAttribute('stroke-dasharray');
+
+const RF = 0.01; // Roughness Factor. Smaller values will make the circle animation appear smoother. Also necessary to keep timer decrement on a real-time-pace and synchronized with the animation.
+let initialTime = null;
+let dashOffset = null;
 let interval = null;
 let isPaused = true;
+let hasInputEventOcurred = false;
 
 function start() {
 	interval = setInterval(() => {
 		tick();
-	}, 10);	// run every 0.01 seconds instead of every second to facilitate smooth animation.
+	}, 1000 * RF);
 	isPaused = false;
+	initialTime = input.value;
+	hasInputEventOcurred = false;
 }
 
 function pause() {
@@ -21,29 +29,41 @@ function pause() {
 
 function reset() {
 	clearInterval(interval);
-	input.value = 10;
+	circle.setAttribute('stroke-dashoffset', 0);
+	isPaused = true;
+	initialTime = null;
+	dashOffset = null;
+	if (!hasInputEventOcurred) input.value = 5;
+	hasInputEventOcurred = false;
 }
 
 function tick() {
 	if (input.value > 0) {
-		console.log('tick');
-		// For the timer to run on normal pace while keeping a smooth animation, decrement the input value by 0.01 to match the tick function being run every 0.01 seconds.
-		input.value = (input.value - 0.01).toFixed(2);
+		input.value = (input.value - RF).toFixed(2); // Remember that ROUGHNESS is in seconds!
+		trimCircumference();
 	} else {
 		clearInterval(interval);
 	}
 }
 
+function onInput() {
+	pause();
+	hasInputEventOcurred = true;
+	reset();
+}
+
 function pressEnter(evt) {
 	if (evt.key === 'Enter') {
 		startPauseBtn.click();
-		startPauseBtn.style.active = true;
 	}
 }
 
-input.addEventListener('input', pause);
+function trimCircumference() {
+	dashOffset -= CIRCUMFERENCE / (initialTime / RF);
+	circle.setAttribute('stroke-dashoffset', dashOffset);
+}
+
+input.addEventListener('input', onInput);
 input.addEventListener('keyup', pressEnter);
 startPauseBtn.addEventListener('click', () => (isPaused ? start() : pause()));
 resetBtn.addEventListener('click', reset);
-
-input.value = 10;
